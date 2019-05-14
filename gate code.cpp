9e7ac961f6b1fd13cf23d81699b0bb624304947c
Aaron7821 <50642055+Aaron7821@users.noreply.github.com>
2019-05-15 11:09:03 +1200

    Update gate code.cpp

--- gate code.cpp
+++ gate code.cpp
@@ -4,10 +4,9 @@
 
 int main(){
 
-connect_to_server(130.195.6.196, 1024);
+connect_to_server("130.195.6.196", 1024);
 send_to_server("Please");
-char password;
-receive_from_server(char message[24])
-password = message;
+char password[24];
+receive_from_server(password);
 send_to_server(password);
 }
