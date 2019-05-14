c25f8b8aa535f3143072b6d87d0cf5b2b5c23e07
Aaron7821 <50642055+Aaron7821@users.noreply.github.com>
2019-05-15 10:46:12 +1200

    Update and rename AVC.cpp to gate code.cpp

---
+++ gate code.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "E101.h"
+#include <time.h>
+#include <unistd.h>
+
+//There are 3 functions in E101 library you can use.
+//Listing 5: network functions
+//int connect_to_server( char server_addr[15],int port);
+//int send_to_server(char message[24]);
+//int receive_from_server(char message[24]);
+//It is about moving around arrays of chars and waiting for the server. 
