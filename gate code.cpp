c7348012a7560977003efcd9d2a4dbe6e22fd9df
Aaron7821 <50642055+Aaron7821@users.noreply.github.com>
2019-05-15 11:06:50 +1200

    Update gate code.cpp

--- gate code.cpp
+++ gate code.cpp
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "E101.h"
 #include <time.h>
-#include <unistd.h>
 
-//There are 3 functions in E101 library you can use.
-//Listing 5: network functions
-//int connect_to_server( char server_addr[15],int port);
-//int send_to_server(char message[24]);
-//int receive_from_server(char message[24]);
-//It is about moving around arrays of chars and waiting for the server. 
+int main(){
+
+connect_to_server(130.195.6.196, 1024);
+send_to_server("Please");
+char password;
+receive_from_server(char message[24])
+password = message;
+send_to_server(password);
+}
