94c7dc231f6be279085a9483e074ab4f0b6d9b69
PandaScout <pokemonpo123@hotmail.com>
2019-05-15 14:47:44 +1200

    Update gate code.cpp

--- gate code.cpp
+++ gate code.cpp
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include "E101.h"
-#include <time.h>
+
 
 int main(){
-
-connect_to_server("130.195.6.196", 1024);
-send_to_server("Please");
+char ip[15]=('1','3','0','.','1','9','5','.','6','.','1','9','6');
+connect_to_server(ip, 1024);
+char message[15]=('P','l','e','a','s','e');
+send_to_server(message);
 char password[24];
 receive_from_server(password);
 send_to_server(password);
