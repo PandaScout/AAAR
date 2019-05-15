9e112e6f851480bb67022fd191a6fbdb4adcdc5f
PandaScout <pokemonpo123@hotmail.com>
2019-05-15 14:49:54 +1200

    Update gate code.cpp

--- gate code.cpp
+++ gate code.cpp
@@ -1,3 +1,26 @@
+//new stuff
+#include "E101.h"
+
+
+int main(){
+char ip[15]="130.195.6.196";
+connect_to_server(ip, 1024);
+char message[15]="Please";
+send_to_server(message);
+char password[24];
+receive_from_server(password);
+send_to_server(password);
+}
+
+
+
+
+
+
+
+
+
+//old stuff
 #include "E101.h"
 
 
